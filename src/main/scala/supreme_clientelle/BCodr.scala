80e37bd72fbf713c39011af75403df2e197790b4
aguestuser <guest.austin@gmail.com>
2015-01-11 04:08:27 -0500

    Add BDecoding#unwrap, tests for BCodr#decodeMap

--- src/main/scala/supreme_clientelle/BCodr.scala
+++ src/main/scala/supreme_clientelle/BCodr.scala
@@ -4,65 +4,57 @@ package supreme_clientelle
  * Created by aguestuser on 1/8/15.
  */
 
-abstract class BDecoding
+sealed abstract class BDecoding
 case class BStr(is: String) extends BDecoding
 case class BInt(is: Int) extends BDecoding
 case class BList(is: List[BDecoding]) extends BDecoding
 case class BMap(is: Map[String, BDecoding]) extends BDecoding
 
+object BDecoding {
+  def unwrap(bdec: BDecoding) : Any = bdec match {
+    case BStr(s) => s
+    case BInt(i) => i
+    case BList(l) => l.foldLeft(List[Any]())((b,a) => b :+ unwrap(a) )
+    case BMap(m) => m.keys.foldLeft(Map[String,Any]())((b,a) => b + Tuple2(a, unwrap(m(a))))
+  }
+}
 object BCodr {
 
-  def decode(str: String) : List[BDecoding] = decode(strToBytes(str))
-  def decode(bytes: List[Byte]) : List[BDecoding] = decodeMany(bytes)._1
+  def decode(str: String) : BDecoding = decode(strToBytes(str))
+  def decode(bytes: List[Byte]) : BDecoding = decodeOne(bytes)._1
 
-  def decodeMany(rem: List[Byte]) : (List[BDecoding], List[Byte]) = rem match {
-    case Nil => (List[BDecoding](), rem)
-    case _ =>
-      val (res, tail) = decodeOne(rem)
-      val (list, newTail) = decodeMany(tail)
-      (res :: list, newTail)
-  }
-
-  def decodeOne(rem: List[Byte]) : (BDecoding, List[Byte]) = rem match {
+  private def decodeOne(rem: List[Byte]) : (BDecoding, List[Byte]) = rem match {
     case 'i' :: tail => decodeInt(tail)
     case n :: tail if n.toChar.isDigit => decodeStr(rem)
     case 'l' :: tail =>  val (l, rest) = decodeList(tail); (BList(l), rest)
     case 'd' :: tail => val (l, rest) = decodeMap(tail); (BMap(l), rest)
     case _ => throw new IllegalArgumentException(
       "Looks like you didn't encode something according to spec... Don't worry!" +
-        "Here's the spec: https://wiki.theory.org/BitTorrentSpecification#Lists"
-    )
-  }
+        "Here's the spec: https://wiki.theory.org/BitTorrentSpecification#Lists") }
 
-  def decodeInt(bytes: List[Byte]) : (BDecoding, List[Byte]) = {
+  private def decodeInt(bytes: List[Byte]) : (BDecoding, List[Byte]) = {
     val (intBytes, _ :: tail) = bytes.span(_.toChar != 'e')
-    (BInt(bytesToStr(intBytes).toInt), tail)
-  }
+    (BInt(bytesToStr(intBytes).toInt), tail) }
 
-  def decodeStr(bytes: List[Byte]) : (BDecoding, List[Byte]) = {
+  private def decodeStr(bytes: List[Byte]) : (BDecoding, List[Byte]) = {
     val (len, _ :: tail) = bytes.span(_.toChar != ':')
     val (strBytes, newTail) = tail.splitAt(bytesToStr(len).toInt)
-    (BStr(bytesToStr(strBytes)), newTail)
-  }
+    (BStr(bytesToStr(strBytes)), newTail) }
 
-  def decodeList(bytes: List[Byte]) : (List[BDecoding], List[Byte]) = bytes match {
+  private def decodeList(bytes: List[Byte]) : (List[BDecoding], List[Byte]) = bytes match {
     case 'e' :: tail => (List[BDecoding](), tail)
     case _ =>
       val (item, tail) = decodeOne(bytes)
       val (list, newTail) = decodeList(tail)
-      (item :: list, newTail)
-  }
+      (item :: list, newTail) }
 
-  def decodeMap(bytes: List[Byte]) : (Map[String, BDecoding], List[Byte]) = bytes match {
+  private def decodeMap(bytes: List[Byte]) : (Map[String, BDecoding], List[Byte]) = bytes match {
     case 'e' :: tail => (Map[String, BDecoding](), tail)
     case _ =>
       val (key: BStr, tail) = decodeOne(bytes)
       val (value: BDecoding, newTail) = decodeOne(tail)
       val (map, newestTail) = decodeMap(newTail)
-      (map + Tuple2(key.is, value), newestTail)
-//      val k = (key.is, value)
-//      (map + k, newestTail)
-  }
+      (map + Tuple2(key.is, value), newestTail) }
 
   // utility
   def strToBytes(str: String) : List[Byte] = str.getBytes.toList
@@ -74,3 +66,13 @@ object BCodr {
   //    * that does decodeOne several times
   //    * would look something like this: def many(p : Parser[A]): Parser[List[A]]
   //    * a type alias for Parser[A] might help
+
+//  def decode(str: String) : List[BDecoding] = decode(strToBytes(str))
+//  def decode(bytes: List[Byte]) : List[BDecoding] = decodeMany(bytes)._1
+//  private def decodeMany(rem: List[Byte]) : (List[BDecoding], List[Byte]) = rem match {
+//    case Nil => (List[BDecoding](), rem)
+//    case _ =>
+//      val (res, tail) = decodeOne(rem)
+//      val (list, newTail) = decodeMany(tail)
+//      (res :: list, newTail)
+//  }
--- src/test/scala/supreme_clientelle/BCodr$Test.scala
+++ src/test/scala/supreme_clientelle/BCodr$Test.scala
@@ -2,31 +2,53 @@ package supreme_clientelle
 
 import org.specs2.mutable.Specification
 import supreme_clientelle.BCodr._
+import supreme_clientelle.BDecoding._
 
 /**
  * Created by aguestuser on 1/9/15.
  */
 class BCodr$Test extends Specification {
-  lazy val encoded = Map[String, List[String]](
-    "ints" -> List[String]("i123e","i-123e", "i0e", "i0123456789e"),// add leading 0 failure case?
-    "strings" -> List[String]("3:foo", "20:austin spencer guest", "10:1234567890"),
-    "lists" -> List[String]("l4:spam3:egge")
-  )
-  lazy val decoded = Map(
-    "ints" -> List[BInt](BInt(123), BInt(-123), BInt(0), BInt(123456789)),
-    "strings" -> List[BStr](BStr("foo"), BStr("austin spencer guest"), BStr("1234567890")),
-    "lists" -> List[BList](BList(List(BStr("spam"),BStr("egg"))))
-  )
 
+  object Encoded {
+    lazy val ints = List("i123e", "i-123e", "i0e", "i0123456789e") // add leading 0 failure case?
+    lazy val strings = List("3:foo", "20:austin spencer guest", "10:1234567890")
+    lazy val lists = List(
+      "li2e4:spam3:egge",
+      "li2e4:spam3:eggli2e4:spam3:eggee",
+      "le")
+    lazy val maps = List(
+      "d3:cow3:moo4:spam4:eggse",
+      "d4:spaml1:a1:bee",
+      "d9:publisher3:bob17:publisher-webpage15:www.example.com18:publisher.location4:homee",
+      "de" ) 
+  }
+  
+  object Decoded {
+    lazy val ints = List(123, -123, 0, 123456789)
+    lazy val strings = List("foo", "austin spencer guest", "1234567890")
+    lazy val lists = List(
+      List(2, "spam", "egg"),
+      List(2, "spam", "egg", List(2, "spam", "egg")),
+      List())
+    lazy val maps = List(
+      Map("cow" -> "moo", "spam" -> "eggs"),
+      Map("spam" -> List("a", "b")),
+      Map("publisher" -> "bob", "publisher-webpage" -> "www.example.com", "publisher.location" -> "home"),
+      Map())
+  }
+  
   "#decode" should {
     "correctly decode ints" in {
-      encoded("ints").map{ decode }.map{ _.head } === decoded("ints") // TO-DO: why can't i call .map( _.is) on this!?
+      Encoded.ints.map(decode).map(unwrap) === Decoded.ints
     }
     "correctly decode strings" in {
-      encoded("strings").map{ decode }.map{ _.head } === decoded("strings")
+      Encoded.strings.map(decode).map(unwrap) === Decoded.strings
     }
     "correctly decode lists" in {
-      encoded("lists").map{ decode }.map{ _.head } === decoded("lists")
+      Encoded.lists.map(decode).map(unwrap) === Decoded.lists
+    }
+    "correctly decode maps" in {
+      Encoded.maps.map(decode).map(unwrap) === Decoded.maps
     }
   }
 }
